63046d4996f39124dd200d38cddc01aa4e5ed744
yannajask <yannajask@gmail.com>
2025-03-03 17:51:22 -0500

    cleanup includes in main

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,7 @@
 #define NCURSES_WIDECHAR 1
 #include "game.h"
-#include "block.h"
 #include <ncursesw/curses.h>
 #include <locale.h>
-#include <iostream>
-#include <sstream>
 
 #define BOARD_HEIGHT 20
 #define BOARD_WIDTH 10
